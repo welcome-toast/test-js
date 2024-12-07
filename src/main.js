529493f6ea28c615ee0f35dbc012051155db1523
Jintae Song <w.jinttao@gmail.com>
2024-12-07 15:40:50 +0900

    Feat: add a selector to track by welcome-toast

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import javascriptLogo from "./javascript.svg";
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector("#app");
+app.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -13,12 +14,12 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="welcome-toast-hl" class="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector(".counter"));
